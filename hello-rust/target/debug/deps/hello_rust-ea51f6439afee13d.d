e9d780e883fbdc534305291439da151a3da4c4f9
t-tajiri <swallow.ava@gmail.com>
2019-04-25 22:50:25 +0900

    first commit

---
+++ hello-rust/target/debug/deps/hello_rust-ea51f6439afee13d.d
@@ -0,0 +1,5 @@
+/Users/apple/project/playground/rust/hello-rust/target/debug/deps/hello_rust-ea51f6439afee13d: src/main.rs
+
+/Users/apple/project/playground/rust/hello-rust/target/debug/deps/hello_rust-ea51f6439afee13d.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/apple/project/playground/rust/hello-rust/target/debug/hello-rust.dSYM: /Users/apple/project/playground/rust/hello-rust/src/main.rs

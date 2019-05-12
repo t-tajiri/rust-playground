d2ddd78676650858e1a01d8ff6cabdc6ec8e45f3
t-tajiri <swallow.ava@gmail.com>
2019-05-12 19:40:03 +0900

    add dining philosophers

---
+++ dining_philosophers/doc/search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=[];
+
+searchIndex["main"]={"doc":E,"i":[],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ dining_philosophers/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["main"] = {"name":"","dirs":[],"files":["main.rs"]};
+createSourceSidebar();

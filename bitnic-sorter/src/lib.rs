57cba393e1849501098de68cfae7f43c0e1e3d31
t-tajiri <swallow.ava@gmail.com>
2019-05-13 22:59:12 +0900

    add functions

--- bitnic-sorter/src/lib.rs
+++ bitnic-sorter/src/lib.rs
@@ -1,3 +1,15 @@
+pub fn sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+    unimplemented!();
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
